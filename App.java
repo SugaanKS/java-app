648d4b534c7ca1ea209a57cde69e8c8a3c7b77ba
SugaanKS <sugaansakthivel@gmail.com>
2025-11-12 11:04:45 +0000

    Task2 commit 1

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Sample Java Program!");
-	System.out.println("Second Line");
-	
+	System.out.println("Second Line is added");	
     }
 }
