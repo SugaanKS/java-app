b22979a680972d516720beeaab13757359bfdb79
SugaanKS <sugaansakthivel@gmail.com>
2025-11-12 09:36:23 +0000

    testing commit after removing

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Sample Java Program!");
 	System.out.println("Second Line");
-	System.out.println("Third Line");
+	
     }
 }
