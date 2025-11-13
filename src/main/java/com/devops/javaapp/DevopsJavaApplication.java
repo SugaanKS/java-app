e96c71451468bc34d297d307a53c6baf4e41421c
SugaanKS <sugaansakthivel@gmail.com>
2025-11-13 11:50:20 +0000

    Final Commit

--- src/main/java/com/devops/javaapp/DevopsJavaApplication.java
+++ src/main/java/com/devops/javaapp/DevopsJavaApplication.java
@@ -20,6 +20,6 @@ public class DevopsJavaApplication {
     @GetMapping("/")
     public String hello() {
         // You can customize this message
-        return "<h1>Hello,From DevOps World!</h1><p>Your Java application was successfully deployed via Jenkins and Ansible .</p>";
+        return "<h1>Hello, DevOps World!</h1><p>Your Java application was successfully deployed via Jenkins and Ansible.</p>";
     }
 }
